b2bbf84df8a8be035a00d3ad4996ba27466cc55e
Chrystian22 <110869145+Chrystian22@users.noreply.github.com>
2024-04-01 08:40:00 -0500

    Aprendiendo svelte

---
+++ src/Counter.svelte
@@ -0,0 +1,49 @@
+<script>
+	export let initialCounter = 0;
+  export let maxCounter = 9;
+
+	let contador = initialCounter
+
+	function handleClick () {
+		contador++
+	}
+
+  $: isEvenOrOdd = contador % 2 === 0 ? 'Is Even' : 'Is Odd'
+  $: if (contador > maxCounter) {
+    contador = maxCounter
+  }
+</script>
+
+<main>
+	<button on:click={handleClick}>Incrementar</button>
+	<span>{contador}</span>
+	<span class="suave">{isEvenOrOdd}</span>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 440px;
+		margin: 0 auto;
+	}
+
+	span {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+	.suave {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2rem;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Movie.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let title, poster, year
+</script>
+
+<style>
+  article {
+    border: 1px solid #eee;
+    border-radius: 4px;
+    padding: 16px;
+  }
+
+  h3 {
+    font-size: 24px;
+    margin-bottom: 4px;
+  }
+</style>
+
+<article>
+  <h3>{title}</h3>
+  <img src={poster} alt={title}>
+  <span>{year}</span>
+  {#if 2020 - year < 5}
+    <i>NUEVA</i>
+  {/if}
+</article>
